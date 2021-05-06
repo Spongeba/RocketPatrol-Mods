87a3b47215eecba8bd234edfd7fefe7fe464ccb0
Spongeba <77239690+Spongeba@users.noreply.github.com>
2021-05-05 21:46:08 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,15 @@
+/* 
+My name is Mengya Qiu, and I used about 3 days to complete this project. 
+Create a new spaceship type that's smaller, moves faster, and is worth more points (20)
+Implement a simultaneous two-player mode (30)
+Create a new scrolling tile sprite for the background (5)
+Create a new rocket artwork (5)
+Implement parallax scrolling (10)
+Replace the UI borders with new artwork (10)
+Create a new sprite for the Spaceship enemies (10)
+Implement mouse control for player movement and mouse click to fire (20)
+*/
+
 let config = {
     type: Phaser.AUTO,
     width: 640,
